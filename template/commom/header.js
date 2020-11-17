85152cd4eecae9f87f094581624023e46ea64ead
antrikshmisri <antrikshmisri@gmail.com>
2020-11-18 02:38:32 +0530

    feat: added register button to signin page

--- template/commom/header.js
+++ template/commom/header.js
@@ -2,13 +2,13 @@ var header = document.querySelector("body");
 
 header.insertAdjacentHTML(
   "afterbegin",
-  '<div class="container-fluid overlay-div">' +
+  '<div class="container-fluid overlay-div justify-content-center">' +
     '<h1 class="tagline" data-aos = "fade-up" data-aos-delay = "500">A customer experience <span>unlike any other</span></h1>' +
     "</div>" +
     "<div class='container navbar-div px-0'>" +
     '<nav class="navbar navbar-expand-lg navbar-light">' +
     '<a class="navbar-brand my-3" href="#"><span>OpenCity</span></a>' +
-    '<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>' +
+    '<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>' +
     '<div class="collapse navbar-collapse my-3" id="navbarNav">' +
     '<ul class="navbar-nav">' +
     '<li class="nav-item">' +
