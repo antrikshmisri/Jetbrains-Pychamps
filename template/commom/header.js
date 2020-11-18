671b6e9fe4ad9195cad228c5da16c10b28a93472
antrikshmisri <antrikshmisri@gmail.com>
2020-11-19 00:29:49 +0530

    feat: added background image

--- template/commom/header.js
+++ template/commom/header.js
@@ -3,7 +3,13 @@ var header = document.querySelector("body");
 header.insertAdjacentHTML(
   "afterbegin",
   '<div class="container-fluid overlay-div justify-content-center">' +
+  '<div class = "container text-center align-items-center"' +
+    '<div class ="row">'+
+    '<div class = "col-lg-12 tagline-div">'+
     '<h1 class="tagline" data-aos = "fade-up" data-aos-delay = "500">A customer experience <span>unlike any other</span></h1>' +
+    '</div>' +
+    '</div>'+
+    '</div>' +
     "</div>" +
     "<div class='container navbar-div px-0'>" +
     '<nav class="navbar navbar-expand-lg navbar-light">' +
--- template/commom/title.js
+++ template/commom/title.js
@@ -1,14 +1,26 @@
 var title = document.querySelector(".tagline");
-
+const overlayDiv = document.querySelector(".overlay-div");
+const taglineDiv = document.querySelector(".tagline-div");
 var sPath = window.location.pathname;
 
 var sPage = sPath.substring(sPath.lastIndexOf("/") + 1);
+
 if (sPage == "customers.html") {
   console.log(sPage);
   title.innerHTML = title.innerHTML.replace(
     "A customer experience <span>unlike any other</span>",
     "Today’s Restaurants Rely on OpenCity"
   );
-} else sPage == "arteevspage.html";
+} else sPage == "landing.html";
 {
+  console.log(sPage)
+  title.innerHTML = title.innerHTML.replace(
+    "A customer experience <span>unlike any other</span>",
+    "The way customers & restaurants <span>communicate has changed<span>"
+  );
+  taglineDiv.insertAdjacentHTML("afterend" , '<div class = "col-lg-12"><button class = "drive-thru">DRIVE THRU PROGRAM</button></div>')
+  overlayDiv.style.minHeight = "85%";
+  overlayDiv.style.backgroundImage = "url('/images/landing_back.jpg')"
+  overlayDiv.style.backgroundSize = "1800px 1200px"
+  overlayDiv.style.backgroundPosition = "center right"
 }
