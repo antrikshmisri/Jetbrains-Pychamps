4a7a849418bfd5af72e4e2dbb7839df5fb722685
BIraj <birag.gpta@gmail.com>
2020-11-22 00:41:24 +0530

    change

--- template/commom/title.js
+++ template/commom/title.js
@@ -11,7 +11,11 @@ if (sPage == "customers.html") {
     "A customer experience <span>unlike any other</span>",
     "Today’s Restaurants Rely on OpenCity"
   );
-
+  var para = document.createElement("P");
+  para.innerHTML =
+    "Our customers have one thing in common...they all use OpenCity to take the customer experience to the next level.";
+  document.querySelector(".heading").style.flexDirection = "column";
+  document.querySelector(".heading").appendChild(para);
 } else if (sPage == "landing.html") {
   console.log(sPage);
   title.innerHTML = title.innerHTML.replace(
@@ -33,6 +37,4 @@ if (sPage == "customers.html") {
     "A customer experience <span>unlike any other</span>",
     "<span> Request Demo </span>"
   );
-
-
 }
