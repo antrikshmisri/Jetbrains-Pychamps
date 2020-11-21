e44c42f813bbf6735d82333fff7ef13ab242522f
Arteev Raina <arteevraina@gmail.com>
2020-11-21 18:12:53 +0530

    feat: firebase auth

--- template/commom/title.js
+++ template/commom/title.js
@@ -11,16 +11,24 @@ if (sPage == "customers.html") {
     "A customer experience <span>unlike any other</span>",
     "Today’s Restaurants Rely on OpenCity"
   );
-} else sPage == "landing.html";
-{
-  console.log(sPage)
+} else if (sPage == "landing.html") {
+  console.log(sPage);
   title.innerHTML = title.innerHTML.replace(
     "A customer experience <span>unlike any other</span>",
     "The way customers & restaurants <span>communicate has changed<span>"
   );
-  taglineDiv.insertAdjacentHTML("afterend" , '<div class = "col-lg-12"><button class = "drive-thru">DRIVE THRU PROGRAM</button></div>')
+  taglineDiv.insertAdjacentHTML(
+    "afterend",
+    '<div class = "col-lg-12"><button class = "drive-thru">DRIVE THRU PROGRAM</button></div>'
+  );
   overlayDiv.style.minHeight = "85%";
-  overlayDiv.style.backgroundImage = "url('/images/landing_back.jpg')"
-  overlayDiv.style.backgroundSize = "1800px 1200px"
-  overlayDiv.style.backgroundPosition = "center right"
+  overlayDiv.style.backgroundImage = "url('/images/landing_back.jpg')";
+  overlayDiv.style.backgroundSize = "1800px 1200px";
+  overlayDiv.style.backgroundPosition = "center right";
+} else if (sPage == "request-demo.html") {
+  console.log(sPage);
+  title.innerHTML = title.innerHTML.replace(
+    "A customer experience <span>unlike any other</span>",
+    "<span> Request Demo </span>"
+  );
 }
---
+++ template/scripts/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzKvGSA0XxzM2828cTofbffSsIHrX_hvM",
+  authDomain: "pycharms-jetbrain-hackathon.firebaseapp.com",
+  databaseURL: "https://pycharms-jetbrain-hackathon.firebaseio.com",
+  projectId: "pycharms-jetbrain-hackathon",
+  storageBucket: "pycharms-jetbrain-hackathon.appspot.com",
+  messagingSenderId: "907943394725",
+  appId: "1:907943394725:web:e5dafcbd9505a1ee85fc4c",
+  measurementId: "G-C8ZSX9R691",
+};
